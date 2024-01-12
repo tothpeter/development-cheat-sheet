017fe85e9a4033f68412628b77524af643cd179a
Peter Toth <tothpeter08@gmail.com>
2024-01-12 17:17:00 +0100

    More precise naming

--- ruby/memory_profiling.rb
+++ ruby/memory_profiling.rb
@@ -2,12 +2,12 @@
 a = []
 "#{ObjectSpace.memsize_of(a)/1024} Kb"
 
-# Collect RSS and Object count on the heap for the current process for drawing 
+# Collect RSS and Object count on the heap for the current process for drawing
 file_name = "mem_stat.data"
 file_path = ENV['HOME'] + "/tmp/memory_reporting/#{file_name}"
 
 open(file_path, 'w') { |f|
-  f.puts "date, RSS, Object count on heap"
+  f.puts "date, RSS(Mbyte), Object count on heap"
   f.puts Time.now.to_i.to_s + ", 0, 0"
 }
 
@@ -15,10 +15,10 @@ Thread.new do
   pid = Process.pid
 
   while true
-    rss = `ps -eo pid,rss | grep #{pid} | awk '{print $2}'`.to_i / 1024
+    rss_in_mbyte = `ps -eo pid,rss | grep #{pid} | awk '{print $2}'`.to_i / 1024
 
     open(file_path, 'a') { |f|
-      f.puts [Time.now.to_i, rss, GC.stat[:heap_live_slots]].join(', ')
+      f.puts [Time.now.to_i, rss_in_mbyte, GC.stat[:heap_live_slots]].join(', ')
     }
 
     sleep 1
